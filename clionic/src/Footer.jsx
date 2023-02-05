92cf4415cfc14d28170f24f7e7d5f5dbfad2f16c
philica <fikretucker79@gmail.com>
2023-02-05 19:53:35 +0300

    create Header and Footer component

---
+++ clionic/src/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+export function Footer({}) {
+  return <div className="nk-footer">
+                    <div className="container-fluid">
+                        <div className="nk-footer-wrap">
+                            <div className="nk-footer-copyright"> &copy; 2020 Clionic. Designed by Kepler Labs
+                            </div>
+                            <div className="nk-footer-links">
+                                <ul className="nav nav-sm">
+                                    <li className="nav-item"><a className="nav-link" href="#">Terms</a></li>
+                                    <li className="nav-item"><a className="nav-link" href="#">Privacy</a></li>
+                                    <li className="nav-item"><a className="nav-link" href="#">Help</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>;
+}
+  
