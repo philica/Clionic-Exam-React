aea618b64cafe3a1c8ca018b48d4833a3f751bfa
philica <fikretucker79@gmail.com>
2023-02-05 19:48:43 +0300

    Create Content component Delete App.css Modify css

---
+++ clionic/src/Content.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+export function Content({}) {
+  return <div className="nk-content ">
+                    <div className="container-fluid">
+                        <div className="nk-content-inner">
+                            <div className="nk-content-body">
+                                <div className="nk-block">
+                                    <div className="row g-gs">
+                                        <div className="col-md-5 col-lg-5 col-xxl-3">
+                                            <div className="nk-block-head nk-block-head-sm">
+                                                <div className="nk-block-between">
+                                                    <div className="nk-block-head-content">
+                                                        <h4 className="">EXAM ROOMS</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="card card-bordered card-preview">
+                                                <div className="card-inner p-0">
+                                                    <table className="table table-hover p-0">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">ROOM ID</th>
+                                                                <th scope="col">ROOM NAME</th>
+                                                                <th scope="col">REG. DATE</th>
+                                                                <th scope="col">ACTION</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr className="p-4">
+                                                                <td scope="row">R.01</td>
+                                                                <td>Sleep Apnea Lab</td>
+                                                                <td>17/10/2022</td>
+                                                                <td>
+                                                                    <div className="card-tools shrink-0 d-none d-sm-block">
+                                                                        <ul className="nav nav-switch-s2 nav-tabs bg-white">
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-edit"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-disk"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-trash"></em></a></li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td scope="row">R.02</td>
+                                                                <td>Narcolepsy Lab</td>
+                                                                <td>17/10/2022</td>
+                                                                <td>
+                                                                    <div className="card-tools shrink-0 d-none d-sm-block">
+                                                                        <ul className="nav nav-switch-s2 nav-tabs bg-white">
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-edit"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-disk"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-trash"></em></a></li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td scope="row">R.03</td>
+                                                                <td>Insomnia Lab</td>
+                                                                <td>17/10/2022</td>
+                                                                <td>
+                                                                    <div className="card-tools shrink-0 d-none d-sm-block">
+                                                                        <ul className="nav nav-switch-s2 nav-tabs bg-white">
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-edit"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-disk"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-trash"></em></a></li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td scope="row">R.04</td>
+                                                                <td>RLS Lab</td>
+                                                                <td>17/10/2022</td>
+                                                                <td>
+                                                                    <div className="card-tools shrink-0 d-none d-sm-block">
+                                                                        <ul className="nav nav-switch-s2 nav-tabs bg-white">
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-edit"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-disk"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-trash"></em></a></li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td scope="row">R.05</td>
+                                                                <td>Sleep Apnea Lab</td>
+                                                                <td>17/10/2022</td>
+                                                                <td>
+                                                                    <div className="card-tools shrink-0 d-none d-sm-block">
+                                                                        <ul className="nav nav-switch-s2 nav-tabs bg-white">
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-edit"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-disk"></em></a></li>
+                                                                            <li className="nav-item"><a href="#" className="nav-link"><em className="icon ni ni-trash"></em></a></li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                          
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>;
+}
+  
